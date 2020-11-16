565730c5781cbc2525622acf384c029a333c2cbf
muzammi06 <74516872+muzammi06@users.noreply.github.com>
2020-11-16 08:04:56 +0700

    Add files via upload

---
+++ scritp1.py
@@ -0,0 +1,14 @@
+# fungsi perkalian
+def perkalian():
+  return perkalian
+
+# Meminta input dari user
+num1 = int(input("Masukkan bilangan pertama: "))
+num2 = int(input("Masukkan bilangan kedua: "))
+hasil = num1 * num2
+
+if hasil >= 1000:
+    print ('Jumlah Perkalian adalah : ', hasil)
+
+else :
+ print ('Maaf tidak Bisa ditampilkan, Karena Kurang dari 1000')
